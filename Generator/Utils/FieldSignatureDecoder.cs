10cfc72df12bcde617d0e19bd432050858f30f18
Tao Beloney <tbeloney@gmail.com>
2025-09-06 14:02:38 -0500

    Factor out SimpleFieldKind

--- Generator/Utils/FieldSignatureDecoder.cs
+++ Generator/Utils/FieldSignatureDecoder.cs
@@ -10,20 +10,6 @@ using System.Text.RegularExpressions;
 using MetadataUtils;
 using Microsoft.VisualBasic.FileIO;
 
-// The kind of field - for AHK we only care whether it's a primitive, pointer, or array (and its type and rank if an array)
-// We don't care about most of the specifics
-public enum SimpleFieldKind
-{
-    Primitive,  // int, int16, uint, etc
-    Pointer,    // any pointer-sized integer (function pointers, COM interface pointers, etc)
-    Array,
-    Struct,     // an embedded struct. TypeDef contains the type of the struct itself in this case
-    Class,
-    Other,      // unhandled, will produce an error
-    COM,        // A pointer to a COM Interface
-    String      // A string buffer for which we can use StrPut / StrGet (usually a character array)
-}
-
 public static class FieldSignatureDecoder
 {
     public static FieldInfo DecodeFieldType(MetadataReader reader, FieldDefinition fieldDef)
