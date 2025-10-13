1c57c9506d48451328b1ff259b2b18ac60589202
Tao Beloney <taorcb@gmail.com>
2025-10-12 21:22:35 -0500

    Add utilities to AhkParameter

--- Generator/model/AhkParameter.cs
+++ Generator/model/AhkParameter.cs
@@ -7,7 +7,9 @@ public enum CustomParamAttributes
     None = 0,
     Reserved = 1,
     Constant = 2,
-    SizedBuffer = 4
+    SizedBuffer = 4,
+    RetVal = 8,
+    ComOutPtr = 16
 }
 
 public readonly record struct AhkParameter
@@ -35,9 +37,36 @@ public readonly record struct AhkParameter
         this.CustomAttributes = CustomAttributes;
     }
 
-    internal bool IsInParam => Attributes.HasFlag(ParameterAttributes.In);
-    internal bool IsOutParam => Attributes.HasFlag(ParameterAttributes.Out);
-    internal bool Optional => Attributes.HasFlag(ParameterAttributes.Optional);
-    internal bool Constant => CustomAttributes.HasFlag(CustomParamAttributes.Constant);
-    internal bool Reserved => CustomAttributes.HasFlag(CustomParamAttributes.Reserved);
+    public bool IsInParam => Attributes.HasFlag(ParameterAttributes.In);
+    public bool IsOutParam => Attributes.HasFlag(ParameterAttributes.Out);
+    public bool Optional => Attributes.HasFlag(ParameterAttributes.Optional);
+    public bool Constant => CustomAttributes.HasFlag(CustomParamAttributes.Constant);
+    public bool Reserved => CustomAttributes.HasFlag(CustomParamAttributes.Reserved);
+    public bool IsReturnValue => CustomAttributes.HasFlag(CustomParamAttributes.RetVal);
+    public bool IsComOutPtr => CustomAttributes.HasFlag(CustomParamAttributes.ComOutPtr);
+
+    public bool IsPtr => FieldInfo.Kind == SimpleFieldKind.Pointer;
+    public bool IsPrimitive => FieldInfo.Kind == SimpleFieldKind.Primitive;
+    public bool IsArray => FieldInfo.Kind == SimpleFieldKind.Array;
+    public bool IsStruct => FieldInfo.Kind == SimpleFieldKind.Struct;
+    public bool IsString => FieldInfo.Kind == SimpleFieldKind.String;
+    public bool IsHRESULT => FieldInfo.Kind == SimpleFieldKind.HRESULT;
+    public bool IsCom => FieldInfo.Kind == SimpleFieldKind.COM;
+    public bool IsClass => FieldInfo.Kind == SimpleFieldKind.Class;
+    public bool IsOther => FieldInfo.Kind == SimpleFieldKind.Other;
+
+    public bool IsPtrToPrimitive => IsPtr && FieldInfo?.Kind == SimpleFieldKind.Primitive;
+
+    public string? GetTypeDefName(MetadataReader mr)
+    {
+        if (FieldInfo == null || FieldInfo.TypeDef == null)
+            return null;
+        return mr.GetString(FieldInfo.TypeDef.Value.Name);
+    }
+    
+    public string? GetTypeDefNamespace(MetadataReader mr) {
+        if (FieldInfo == null || FieldInfo.TypeDef == null)
+            return null;
+        return mr.GetString(FieldInfo.TypeDef.Value.Namespace);
+    }
 }
