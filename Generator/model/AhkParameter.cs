holy-tao/AhkWin32Structs-Generator

commit b8c881155cdacffd34f526c689d3243aaaf32e28

Use TypeDefinition reader to parse parameter information
I changed the type resolution logic a while ago to bundle a FieldDef's
MetadataReader with it's type information, but most places that need to
access type information didn't use this MetadataReader. This was fine
when the only assembly being read was Win32, but with WDK and in the
future when WinRT assemblies are added, we need to use the correct
MetadataReader for the assembly that the type is into avoid exceptions.

Generator/model/AhkParameter.cs
@@ -91,31 +91,35 @@ public readonly record struct AhkParameter
 
     public bool IsPtrToString => IsPtr && (FieldInfo.UnderlyingType?.Kind is SimpleFieldKind.String);
 
-    public bool IsPtrToHandle(MetadataReader mr)
+    public bool IsPtrToHandle()
     {
-        if (!IsPtr || FieldInfo.UnderlyingType?.TypeDef == null)
+        if (!IsPtr || FieldInfo.UnderlyingType?.TypeDef == null || FieldInfo.Reader == null)
             return false;
 
-        return AhkStruct.IsHandle(mr, FieldInfo.UnderlyingType?.TypeDef ?? throw new NullReferenceException());
+        return AhkStruct.TypeIsHandle(
+            FieldInfo.Reader, 
+            FieldInfo.UnderlyingType?.TypeDef ?? throw new NullReferenceException()
+        );
     }
 
-    public bool IsHandle(MetadataReader mr)
+    public bool IsHandle()
     {
         if (!FieldInfo.TypeDef.HasValue)
             return false;
-        return AhkStruct.IsHandle(mr, FieldInfo.TypeDef.Value);
+        return AhkStruct.TypeIsHandle(FieldInfo.Reader ?? throw new NullReferenceException(nameof(FieldInfo.Reader))
+            , FieldInfo.TypeDef.Value);
     }
 
-    public string? GetTypeDefName(MetadataReader mr)
+    public string? GetTypeDefName()
     {
-        if (FieldInfo == null || FieldInfo.TypeDef == null)
+        if (FieldInfo == null || FieldInfo.TypeDef == null || FieldInfo.Reader == null)
             return null;
-        return mr.GetString(FieldInfo.TypeDef.Value.Name);
+        return FieldInfo.Reader.GetString(FieldInfo.TypeDef.Value.Name);
     }
     
-    public string? GetTypeDefNamespace(MetadataReader mr) {
-        if (FieldInfo == null || FieldInfo.TypeDef == null)
+    public string? GetTypeDefNamespace() {
+        if (FieldInfo == null || FieldInfo.TypeDef == null || FieldInfo.Reader == null)
             return null;
-        return mr.GetString(FieldInfo.TypeDef.Value.Namespace);
+        return FieldInfo.Reader.GetString(FieldInfo.TypeDef.Value.Namespace);
     }
 }

Generator/Utils/FieldInfo.cs
@@ -221,4 +221,21 @@ public record FieldInfo(SimpleFieldKind Kind, string TypeName, int Length = 0, T
         return Reader?.GetString(TypeDef?.Namespace ?? throw new NullReferenceException(nameof(TypeDef)))
             ?? throw new NullReferenceException(nameof(Reader));
     }
+
+    public string GetTypeDefFqn()
+    {
+        if(Reader is null)
+            throw new NullReferenceException(nameof(Reader));
+        if(TypeDef is null)
+            throw new NullReferenceException(nameof(TypeDef));
+
+        return $"{Reader.GetString(TypeDef.Value.Namespace)}.{Reader.GetString(TypeDef.Value.Name)}";
+    }
+
+    public string GetUnderlyingTypeFqn()
+    {
+        if(UnderlyingType is null)
+            throw new NullReferenceException(nameof(UnderlyingType));
+        return UnderlyingType.GetTypeDefFqn();
+    }
 }

Generator/Utils/FieldSignatureDecoder.cs
@@ -141,7 +141,7 @@ public static class FieldSignatureDecoder
     public static bool IsNonHandleNativeTypedef(MetadataReader mr, TypeDefinition typeDef)
     {
         return CustomAttributeDecoder.GetAllNames(mr, typeDef).Contains("NativeTypedefAttribute")
-            && !AhkStruct.IsHandle(mr, typeDef)
+            && !AhkStruct.TypeIsHandle(mr, typeDef)
             && typeDef.GetFields().Count == 1;
     }
 

Generator/model/AhkComMethod.cs
@@ -9,7 +9,7 @@ class AhkComMethod : AhkMethod
 {
     public int VTableIndex { get; private set; }
 
-    public bool HasStringParam => parameters[1..].Any(p => p.GetTypeDefName(mr) is "BSTR");
+    public bool HasStringParam => parameters[1..].Any(p => p.GetTypeDefName() is "BSTR");
 
     public bool IsSpecialName => methodDef.Attributes.HasFlag(MethodAttributes.SpecialName);
 
@@ -67,7 +67,7 @@ class AhkComMethod : AhkMethod
 
         foreach (AhkParameter param in parameters[1..])
         {
-            string? typeName = param.GetTypeDefName(mr);
+            string? typeName = param.GetTypeDefName();
 
             if (typeName is "BSTR")
             {
@@ -77,7 +77,7 @@ class AhkComMethod : AhkMethod
             {
                 conversions.AppendLine($"        {param.Name} := {param.Name} is String ? StrPtr({param.Name}) : {param.Name}");
             }
-            else if (param.IsHandle(mr))
+            else if (param.IsHandle())
             {
                 conversions.AppendLine($"        {param.Name} := {param.Name} is Win32Handle ? NumGet({param.Name}, \"ptr\") : {param.Name}");
             }
@@ -149,7 +149,7 @@ class AhkComMethod : AhkMethod
         {
             IEnumerable<AhkParameter> candidateParams = parameters
                 .Where(p => p.IsOutParam && !p.IsInParam)
-                .Where(p => p.IsPtrToPrimitive || p.IsPtrToStruct || p.IsPtrToCom || p.IsPtrToHandle(mr));
+                .Where(p => p.IsPtrToPrimitive || p.IsPtrToStruct || p.IsPtrToCom || p.IsPtrToHandle());
             if (candidateParams.Count() == 1)
             {
                 outParam = candidateParams.Single();

Generator/model/AhkConstant.cs
@@ -34,8 +34,10 @@ class AhkConstant
 
         if (IsStruct)
         {
-            decodedStruct = AhkStruct.Get(mr, fieldInfo.TypeDef ?? throw new NullReferenceException())
-                ?? throw new NullReferenceException();
+            decodedStruct = AhkStruct.Get(
+                fieldInfo.Reader ?? throw new NullReferenceException(nameof(fieldInfo.Reader)), 
+                fieldInfo.TypeDef ?? throw new NullReferenceException(nameof(fieldInfo.TypeDef))
+            ) ?? throw new NullReferenceException();
         }
     }
 
@@ -66,7 +68,10 @@ class AhkConstant
 
     public void ToAhkStruct(StringBuilder sb)
     {
-        if (AhkStruct.IsHandle(mr, decodedStruct!.typeDef))
+        if(decodedStruct is null)
+            throw new NullReferenceException(nameof(decodedStruct));
+
+        if (decodedStruct.IsHandle)
         {
             // Constant handles are not owned by the caller
             sb.AppendLine($"    static {Name} => {decodedStruct.Name}({{Value: {GetValueAsAhk()}}}, false)");
@@ -219,11 +224,8 @@ class AhkConstant
         List<string> referencedTypes = [];
         if (IsStruct && !IsGuid)
         {
-            TypeDefinition structType = fieldInfo.TypeDef ?? throw new NullReferenceException(nameof(fieldInfo.TypeDef));
-
-            string fqn = AhkType.GetFqn(mr, structType);
-            referencedTypes.AddRange(decodedStruct!.GetReferencedTypes());
-            referencedTypes.Add(fqn);
+            referencedTypes.AddRange(decodedStruct.GetReferencedTypes());
+            referencedTypes.Add(fieldInfo.GetTypeDefFqn());
         }
 
         return referencedTypes;

Generator/model/AhkMethod.cs
@@ -200,7 +200,7 @@ class AhkMethod
             // bytesParamIndex + 1 because we include the return value as a param
             sb.AppendLine($"        {outParam.Name} := Buffer({parameters[bytesParamIndex + 1].Name}, 0)");
         }
-        else if (outParam.IsPtrToStruct || outParam.IsPtrToHandle(mr))
+        else if (outParam.IsPtrToStruct || outParam.IsPtrToHandle())
         {
             sb.AppendLine($"        {outParam.Name} := {outParam.FieldInfo.UnderlyingType?.TypeName}()");
         }
@@ -216,7 +216,7 @@ class AhkMethod
         AhkParameter fnRetVal = outputParameter ?? parameters[0];
 
         // We need to wrap handles and decide ownership & validity
-        if (fnRetVal.IsHandle(mr))
+        if (fnRetVal.IsHandle())
         {
             TypeDefinition returnValueType = fnRetVal.FieldInfo.TypeDef ?? throw new NullReferenceException();
 
@@ -230,8 +230,10 @@ class AhkMethod
                 sb.AppendLine();
             }
 
-            string fieldName = mr.GetString(mr.GetFieldDefinition(returnValueType.GetFields().First()).Name);
-            sb.AppendLine($"        resultHandle := {fnRetVal.GetTypeDefName(mr)}({{{fieldName}: {fnRetVal.Name}}}, {fnRetVal.ScriptOwned})");
+            MetadataReader retValReader = fnRetVal.FieldInfo.Reader ?? throw new NullReferenceException(nameof(FieldInfo.Reader));
+            var handleField = retValReader.GetFieldDefinition(returnValueType.GetFields().First());
+            string fieldName = retValReader.GetString(handleField.Name);
+            sb.AppendLine($"        resultHandle := {fnRetVal.GetTypeDefName()}({{{fieldName}: {fnRetVal.Name}}}, {fnRetVal.ScriptOwned})");
             if (fnRetVal.HasRAIIFree)
             {
                 // Destructor for RAIIFree is in this namespace - not necessarily true for FreeWith
@@ -256,13 +258,13 @@ class AhkMethod
 
         foreach (AhkParameter param in parameters[1..].Where(p => !p.Reserved && p != outputParameter))
         {
-            string? typeName = param.GetTypeDefName(mr);
+            string? typeName = param.GetTypeDefName();
 
             if (typeName is "PSTR" or "PWSTR")
             {
                 conversions.AppendLine($"        {param.Name} := {param.Name} is String ? StrPtr({param.Name}) : {param.Name}");
             }
-            else if (param.IsHandle(mr))
+            else if (param.IsHandle())
             {
                 conversions.AppendLine($"        {param.Name} := {param.Name} is Win32Handle ? NumGet({param.Name}, \"ptr\") : {param.Name}");
             }
@@ -302,7 +304,7 @@ class AhkMethod
         AhkParameter outParam = default;
         IEnumerable<AhkParameter> candidateParams = parameters
             .Where(p => p.IsOutParam && !p.IsInParam)
-            .Where(p => p.IsPtrToPrimitive || p.IsPtrToHandle(mr) || p.IsPtrToCom);     // Only consider scalar, handle, and com output pointers
+            .Where(p => p.IsPtrToPrimitive || p.IsPtrToHandle() || p.IsPtrToCom);     // Only consider scalar, handle, and com output pointers
         if (candidateParams.Count() == 1)
         {
             outParam = candidateParams.Single();
@@ -331,10 +333,12 @@ class AhkMethod
         }
 
         // If the return type is a handle, we need to import the handle
-        if (FuncHasReturnValue && parameters[0].IsHandle(mr))
+        if (FuncHasReturnValue && parameters[0].IsHandle())
         {
-            referencedTypes.Add(AhkType.GetFqn(mr, parameters[0].FieldInfo.TypeDef 
-                ?? throw new NullReferenceException()));
+            referencedTypes.Add(AhkType.GetFqn(
+                parameters[0].FieldInfo.Reader ?? throw new NullReferenceException(nameof(FieldInfo.Reader)),
+                parameters[0].FieldInfo.TypeDef ?? throw new NullReferenceException(nameof(FieldInfo.TypeDef)))
+            );
         }
 
         // If we have an output parameter, import its type if it's in the Win32Metadata
@@ -434,7 +438,7 @@ class AhkMethod
         {
             AhkParameter param = parameters[i];
 
-            bool isString = param.FieldInfo.TypeDef.HasValue && mr.GetString(param.FieldInfo.TypeDef.Value.Name) is "PWSTR" or "PSTR";
+            bool isString = param.GetTypeDefName() is "PWSTR" or "PSTR";
             string dllCallType = isString ? "ptr" : param.FieldInfo.GetDllCallType(false);
 
             string marshalAs = (param.IsPtrToPrimitive && !param.Reserved && param != outputParameter) ? $"{param.Name}Marshal" : $"\"{dllCallType}\"";

Generator/model/AhkStruct.cs
@@ -11,6 +11,8 @@ public partial class AhkStruct : AhkType
 
     public bool IsUnion => flags.HasFlag(MemberFlags.Union);
 
+    public bool IsHandle => TypeIsHandle(mr, typeDef);
+
     public virtual void ToAhk(StringBuilder sb, bool headers, List<AhkStructMember> emittedMembers)
     {
         HeadersToAhk(sb);
@@ -42,6 +44,7 @@ public partial class AhkStruct : AhkType
     {
         var imports = base.GetReferencedTypes();
 
+        // TODO need to resolve TypeReferences in here
         IEnumerable<string> embeddedImports = GetAllNonNestedMembers()
             .Where(m => m.embeddedStruct != null)
             .Where(m =>
@@ -52,7 +55,17 @@ public partial class AhkStruct : AhkType
                 !(m.fieldInfo.TypeDef?.IsNested ?? false)
             )
             .GroupBy(m => m.fieldInfo.TypeName) // prevent duplicates
-            .Select(g => GetFqn(mr, g.First().fieldInfo.TypeDef ?? throw new NullReferenceException()));
+            .Select(grp =>
+            {
+                FieldInfo typeInfo = grp.First().fieldInfo;
+                return typeInfo.Kind switch
+                {
+                    SimpleFieldKind.Array => typeInfo.GetUnderlyingTypeFqn(),
+                    _ => typeInfo.GetTypeDefFqn()
+                };
+            });
+
+            //GetFqn(g.First()!.fieldInfo!.Reader!, g.First().fieldInfo.TypeDef ?? throw new NullReferenceException()));
 
         imports.AddRange(embeddedImports.Distinct());
         return imports;

Generator/model/AhkStructFactory.cs
@@ -23,7 +23,7 @@ public partial class AhkStruct : AhkType
             }
         }
 
-        return IsHandle(reader, typeDef) ?
+        return TypeIsHandle(reader, typeDef) ?
             new AhkHandle(reader, typeDef) :
             new AhkStruct(reader, typeDef);
             
@@ -144,7 +144,7 @@ public partial class AhkStruct : AhkType
     /// Try to figure out whether or not this struct is a handle type.
     /// </summary>
     /// <returns></returns>
-    public static bool IsHandle(MetadataReader mr, TypeDefinition td)
+    public static bool TypeIsHandle(MetadataReader mr, TypeDefinition td)
     {
         IEnumerable<string> attrs = CustomAttributeDecoder.GetAllNames(mr, td);
         return td.GetFields().Count == 1 && attrs.Any(HandleAttrs.Contains);

commit 912d96652768f3fbf80f70055bde428e0e163474

Fix handle detection for method output parameters
b8c8811 broke handle detection because it used the wrong MetadataReader
to check parameters' underlying types.

Generator/model/AhkParameter.cs
@@ -180,11 +180,11 @@ public readonly record struct AhkParameter
 
     public bool IsPtrToHandle()
     {
-        if (!IsPtr || FieldInfo.UnderlyingType?.TypeDef == null || FieldInfo.Reader == null)
+        if (!IsPtr || FieldInfo.UnderlyingType?.TypeDef is null || FieldInfo.UnderlyingType?.Reader is null)
             return false;
 
         return AhkStruct.TypeIsHandle(
-            FieldInfo.Reader, 
+            FieldInfo.UnderlyingType?.Reader ?? throw new NullReferenceException(), 
             FieldInfo.UnderlyingType?.TypeDef ?? throw new NullReferenceException()
         );
     }